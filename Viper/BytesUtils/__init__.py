7cf1e3108a6a1e14e00b9a60963e33bc11cd5c42
HeapHeapHooray <59413175+HeapHeapHooray@users.noreply.github.com>
2025-03-25 23:02:22 -0300

    Add files via upload

--- Viper/BytesUtils/__init__.py
+++ Viper/BytesUtils/__init__.py
@@ -13,6 +13,7 @@ BytesPack._type_minimum_sizes["vec3"] = 12
 BytesPack._type_minimum_sizes["unit_quaternion"] = 12
 BytesPack._type_minimum_sizes["variable1"] = 1
 BytesPack._type_minimum_sizes["variable2"] = 2
+BytesPack._type_minimum_sizes["fixed32"] = 32
 
 def _pack_vector3(vec3):
     return vec3.convert_to_bytes()
@@ -24,6 +25,8 @@ def _pack_variable2_be(data: bytes):
     return pack_bytes_big_endian(["unsigned int16"],len(data))+data
 def _pack_variable2_le(data: bytes):
     return pack_bytes_little_endian(["unsigned int16"],len(data))+data
+def _pack_fixed32(data: bytes):
+    return data
 
 
 BytesPack._pack_function_dict_be["vector3"] = _pack_vector3
@@ -31,11 +34,13 @@ BytesPack._pack_function_dict_be["vec3"] = _pack_vector3
 BytesPack._pack_function_dict_be["unit_quaternion"] = _pack_unitary_quaternion
 BytesPack._pack_function_dict_be["variable1"] = _pack_variable1
 BytesPack._pack_function_dict_be["variable2"] = _pack_variable2_be
+BytesPack._pack_function_dict_be["fixed32"] = _pack_fixed32
 BytesPack._pack_function_dict_le["vector3"] = _pack_vector3
 BytesPack._pack_function_dict_le["vec3"] = _pack_vector3
 BytesPack._pack_function_dict_le["unit_quaternion"] = _pack_unitary_quaternion
 BytesPack._pack_function_dict_le["variable1"] = _pack_variable1
 BytesPack._pack_function_dict_le["variable2"] = _pack_variable2_le
+BytesPack._pack_function_dict_le["fixed32"] = _pack_fixed32
 
 def _unpack_vector3(data: bytes):
     import Maths
@@ -60,6 +65,8 @@ def _unpack_variable2_le(data: bytes):
     unpacked_data,remaining = unpack_bytes_little_endian(["unsigned int16"],data)
     size = unpacked_data[0]
     return (remaining[0:size],remaining[size:])
+def _unpack_fixed32(data: bytes):
+    return (data[0:32],data[32:])
 
 
 BytesPack._unpack_function_dict_be["vector3"] = _unpack_vector3
@@ -67,9 +74,11 @@ BytesPack._unpack_function_dict_be["vec3"] = _unpack_vector3
 BytesPack._unpack_function_dict_be["unit_quaternion"] = _unpack_unitary_quaternion
 BytesPack._unpack_function_dict_be["variable1"] = _unpack_variable1
 BytesPack._unpack_function_dict_be["variable2"] = _unpack_variable2_be
+BytesPack._unpack_function_dict_be["fixed32"] = _unpack_fixed32
 BytesPack._unpack_function_dict_le["vector3"] = _unpack_vector3
 BytesPack._unpack_function_dict_le["vec3"] = _unpack_vector3
 BytesPack._unpack_function_dict_le["unit_quaternion"] = _unpack_unitary_quaternion
 BytesPack._unpack_function_dict_le["variable1"] = _unpack_variable1
 BytesPack._unpack_function_dict_le["variable2"] = _unpack_variable2_le
+BytesPack._unpack_function_dict_le["fixed32"] = _unpack_fixed32
 
