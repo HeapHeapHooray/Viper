7fda89e506f3fff172ba940543518bcb838a207a
heapheaphooray <heapheaphooray@hotmail.com>
2025-04-14 13:06:12 -0300

    Added network module.

---
+++ Viper/Network/IncomingDataRuntime.py
@@ -0,0 +1,20 @@
+import threading
+from .SimulatorConnectionHandler import SimulatorConnectionHandler
+
+class IncomingDataRuntime:
+    def __init__(self,connection_handler: SimulatorConnectionHandler):
+        self._connection_handler = connection_handler
+        self._receive_data_thread = threading.Thread(target=self._receive_data)
+        self._running = False
+    def start(self):
+        self._running = True
+        self._receive_data_thread.start()
+    def stop(self):
+        self._running = False
+        self._receive_data_thread.stop()
+    def is_running(self):
+        return self._running
+    def _receive_data(self):
+        while self._running:
+            print(self._connection_handler.receive_data())
+        
